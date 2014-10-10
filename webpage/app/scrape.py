0da346bf4faa68f6b85815fbf95272d5cfe24813
Henry Wilson <henryhwilson@gmail.com>
2014-10-10 00:22:58 -0400

    Scraper page shows listserv now, was broken

--- webpage/app/scrape.py
+++ webpage/app/scrape.py
@@ -4,4 +4,4 @@ from bs4 import BeautifulSoup
 def get_content():
 	r = requests.get('https://listserv.dartmouth.edu/scripts/wa.exe?A0=CAMPUS-EVENTS')
 	soup = BeautifulSoup(r.text)
-	return(soup.ul)
+	return soup.prettify()
