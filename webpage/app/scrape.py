124cd1ddf9c5dc6b8f62d1775874d7c8e7182552
Henry Wilson <henryhwilson@gmail.com>
2014-10-10 00:13:00 -0400

    Added backend scraping utilities. Added scrape page that displays listserv events, will parse later.

---
+++ webpage/app/scrape.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_content():
+	r = requests.get('https://listserv.dartmouth.edu/scripts/wa.exe?A0=CAMPUS-EVENTS')
+	soup = BeautifulSoup(r.text)
+	return(soup.ul)
