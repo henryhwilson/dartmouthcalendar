f8bb11d3507301e0285d7dabb36134205ca5ff50
Henry Wilson <henryhwilson@gmail.com>
2014-10-31 16:18:12 -0400

    All data now comes through variables in views.py. Sorts based on today/tomorrow/upcoming.

--- webpage/app/views.py
+++ webpage/app/views.py
@@ -16,23 +16,45 @@ def index():
 	categories.append('Misc')
 	events = []
 	event1 = {'from':'Beta Alpha Omega','subject':'18s: Dance Show Tonight @ Beta Alpha Omega',
-	'category':'Greek','date_event':'','date_blitzed':''} 
+	'category':'Greek','time_event':'7PM','date_event':'today'} 
 	events.append(event1)
-	event2 = {'from':'Sigma Phi Epsilon','subject':'Fusion @ SigEp Tonight!','category':'Greek','date_event':'','date_blitzed':''}
+	event2 = {'from':'Sigma Phi Epsilon','subject':'Fusion @ SigEp Tonight!','category':'Greek','time_event':'10PM','date_event':'today'}
 	events.append(event2)
-	event3 = {'from':'Collis Governing Board','subject':'HAUNTED HOUSE DANCE','category':'Social','date_event':'','date_blitzed':''}
+	event3 = {'from':'Collis Governing Board','subject':'HAUNTED HOUSE DANCE','category':'Social','time_event':'8PM 11/4','date_event':'upcoming'}
 	events.append(event3)
+	event4 = {'from':'Kappa Kappa Kappa','subject':'Tri-Kap Dance Party @ 11:30','category':'Greek','time_event':'11:30PM','date_event':'tomorrow'}
+	events.append(event4)
+	event5 = {'from':'Dartmouth Classical Ballet Theatre','subject':'open ballet class on Saturday!','category':'Misc','time_event':'10-11:30AM','date_event':'tomorrow'}
+	events.append(event5)
 	nicknames = {'Delta Delta Delta':'TriDelt','Kappa Kappa Gamma':'KKG','Alpha Delta':'AD',
 	'Sigma Phi Epsilon':'SigEp','Alpha Chi Alpha':'AXA','Beta Alpha Omega':'Beta','Chi Heorot':'Heorot',
-	'Collis Governing Board':'One Wheelock'}
-	cat_freq = {'Greek':0,'Social':0,'Sports':0,'Misc':0}
+	'Collis Governing Board':'One Wheelock','Kappa Kappa Kappa':'Tri-Kap'}
+	today_total_events = 0
+	today_cat_freq = {'Greek':0,'Social':0,'Sports':0,'Misc':0}
+	tomorrow_total_events = 0
+	tomorrow_cat_freq = {'Greek':0,'Social':0,'Sports':0,'Misc':0}
+	upcoming_total_events = 0
+	upcoming_cat_freq = {'Greek':0,'Social':0,'Sports':0,'Misc':0}
 	for event in events:
-		cat_freq[event['category']] += 1
+		if (event['date_event'] == 'today'):
+			today_cat_freq[event['category']] += 1
+			today_total_events += 1
+		elif (event['date_event'] == 'tomorrow'):
+			tomorrow_cat_freq[event['category']] += 1
+			tomorrow_total_events += 1
+		elif (event['date_event'] == 'upcoming'):
+			upcoming_cat_freq[event['category']] += 1
+			upcoming_total_events += 1
+		if (not event['from'] in nicknames.keys()):
+			nicknames[event['from']] = event['from']
 	if hourNow < 19 and hourNow > 4: # Display "Tonight" if between 7:00PM and 4:59AM
 		isDay = True
 	else:
 		isDay = False
-	return render_template('index.html', is_day=isDay, categories=categories, cat_freq=cat_freq, events=events, nicknames=nicknames)
+	return render_template('index.html', is_day=isDay, categories=categories, today_cat_freq=today_cat_freq, 
+		tomorrow_cat_freq=tomorrow_cat_freq, upcoming_cat_freq=upcoming_cat_freq,
+		events=events, today_total_events=today_total_events, tomorrow_total_events=tomorrow_total_events,
+		upcoming_total_events=upcoming_total_events, nicknames=nicknames)
 
 @app.route('/scraper', methods=['GET'])
 def scraper():
