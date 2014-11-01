e259fa66e75af95d2c81c4bf9a1440fae711a66c
Eric Chen <eric.t.chen.17@dartmouth.edu>
2014-11-01 12:31:24 -0400

    can dynamically get the current week and scrapes the previous week as well

--- webpage/app/scrape.py
+++ webpage/app/scrape.py
@@ -8,14 +8,33 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 def get_content():
+	# Get the month and week of month of today.
 	date = datetime.now().date()
 	year = date.year
-	year = year % 2000
+	year = year % 2000  # only the last two digits of year.
 	month = date.month
 	padded_month = ("%02d" % month)
 	day = date.day
+	week_of_month = (day + 6/7)
+	letter_for_week = chr(ord('a') + week_of_month - 1) # first week is A, etc.
 
-	listserv_url = 'https://listserv.dartmouth.edu/scripts/wa.exe?A1=ind' + str(year) + str(padded_month) + 'A&L=CAMPUS-EVENTS&O=D&H=0&D=1&T=1'
+	last_year = year
+	last_month = month
+	last_week = week_of_month - 1
+	# Get the previous week.
+	if week_of_month == 1:
+		last_month = month - 1
+		last_week = 5
+		if month == 1:
+			last_month = 12
+			last_year = year - 1
+		if last_month == 2:
+			last_week = 4
+	padded_last_month = ("%02d" % last_month)
+	letter_for_last_week = chr(ord('a') + last_week - 1) # first week is A, etc.
+
+	# Scrape for this week.
+	listserv_url = 'https://listserv.dartmouth.edu/scripts/wa.exe?A1=ind' + str(year) + str(padded_month) + letter_for_week + '&L=CAMPUS-EVENTS&O=D&H=0&D=1&T=1'
 
 	r = requests.get(listserv_url)
 	soup = BeautifulSoup(r.text)
@@ -30,6 +49,31 @@ def get_content():
    				r = requests.get('https://listserv.dartmouth.edu'+href)
    				soup2 = BeautifulSoup(r.text)
    				for event in soup2.find_all('a'):
+   					if event.get('href'):
+   						if '/scripts/wa.exe?A2=' in event.get('href'):
+   							data = []
+   							data.append(event.text)
+   							data.append(urllib.quote_plus(event.get('href')))
+   							events.append(data)
+   				iterator = iterator + 1
+
+   	# Scrape for last week
+   	# NOTE: I will move this soup/link/event/iterator stuff into a separate function,
+   	# once I know it is okay to do that.
+   	listserv_url2 = 'https://listserv.dartmouth.edu/scripts/wa.exe?A1=ind' + str(last_year) + str(padded_last_month) + letter_for_last_week + '&L=CAMPUS-EVENTS&O=D&H=0&D=1&T=1'
+
+	r = requests.get(listserv_url2)
+	soup3 = BeautifulSoup(r.text)
+	iterator = 0
+	for link in soup3.find_all('a'):
+		if iterator > 0:
+			break
+		href = link.get('href')
+		if href:
+			if  '/scripts/wa.exe?A1=' in href:
+   				r = requests.get('https://listserv.dartmouth.edu'+href)
+   				soup4 = BeautifulSoup(r.text)
+   				for event in soup4.find_all('a'):
    					if event.get('href'):
    						if '/scripts/wa.exe?A2=' in event.get('href'):
    							data = []
